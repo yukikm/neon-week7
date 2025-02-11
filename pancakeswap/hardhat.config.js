d44ac1cce4b7710a66f804a5aae1bca66a21eae0
Igor Prokopev <ip@neonlabs.org>
2025-02-11 13:46:20 +0700

    Codebase format

--- pancakeswap/hardhat.config.js
+++ pancakeswap/hardhat.config.js
@@ -1,84 +1,84 @@
-require("@nomicfoundation/hardhat-ethers")
-require("dotenv").config()
+require('@nomicfoundation/hardhat-ethers');
+require('dotenv').config();
 
 module.exports = {
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.4",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 99999,
-                    },
-                },
-            },
-            {
-                version: "0.6.6",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 99999,
-                    },
-                },
-            },
-            {
-                version: "0.5.16",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 99999,
-                    },
-                },
-            },
-            {
-                version: "0.4.26",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 99999,
-                    },
-                },
-            },
-            {
-                version: "0.4.18",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 99999,
-                    },
-                },
-            },
-        ],
-    },
-    networks: {
-        neondevnet: {
-            url: "https://devnet.neonevm.org",
-            accounts: [process.env.DEPLOYER_KEY],
-            chainId: 245022926,
-            allowUnlimitedContractSize: false,
-            gasMultiplier: 2,
-            maxFeePerGas: '10000000000000',
-            maxPriorityFeePerGas: '5000000000000'
-        },
-        neonmainnet: {
-            url: "https://neon-proxy-mainnet.solana.p2p.org",
-            accounts: [process.env.DEPLOYER_KEY],
-            chainId: 245022934,
-            allowUnlimitedContractSize: false,
-            gas: "auto",
-            gasPrice: "auto"
-        },
-        curvestand: {
-            url: process.env.NEON_EVM_NODE,
-            accounts: [process.env.DEPLOYER_KEY],
-            allowUnlimitedContractSize: false,
-            gasMultiplier: 2,
-            maxFeePerGas: 10000,
-            maxPriorityFeePerGas: 5000
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999
+          }
         }
+      },
+      {
+        version: '0.6.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999
+          }
+        }
+      },
+      {
+        version: '0.5.16',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999
+          }
+        }
+      },
+      {
+        version: '0.4.26',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999
+          }
+        }
+      },
+      {
+        version: '0.4.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999
+          }
+        }
+      }
+    ]
+  },
+  networks: {
+    neondevnet: {
+      url: 'https://devnet.neonevm.org',
+      accounts: [process.env.DEPLOYER_KEY],
+      chainId: 245022926,
+      allowUnlimitedContractSize: false,
+      gasMultiplier: 2,
+      maxFeePerGas: '10000000000000',
+      maxPriorityFeePerGas: '5000000000000'
     },
-    mocha: {
-        timeout: 2800000
+    neonmainnet: {
+      url: 'https://neon-proxy-mainnet.solana.p2p.org',
+      accounts: [process.env.DEPLOYER_KEY],
+      chainId: 245022934,
+      allowUnlimitedContractSize: false,
+      gas: 'auto',
+      gasPrice: 'auto'
+    },
+    curvestand: {
+      url: process.env.NEON_EVM_NODE,
+      accounts: [process.env.DEPLOYER_KEY],
+      allowUnlimitedContractSize: false,
+      gasMultiplier: 2,
+      maxFeePerGas: 10000,
+      maxPriorityFeePerGas: 5000
     }
-}
+  },
+  mocha: {
+    timeout: 2800000
+  }
+};
--- pancakeswap/scripts/pancakeswap-demo-setup.js
+++ pancakeswap/scripts/pancakeswap-demo-setup.js
@@ -1,84 +1,84 @@
-const { ethers, network, run} = require("hardhat")
-const { deployPancakeswapExchange } = require("./deploy-pancakeswap-exchange")
-const { deployERC20ForSPLMintable } = require("./deploy-tokens")
-const { createPairAndAddLiquidity } = require("./create-liquidity-pools")
-const {airdropNEON} = require("./utils");
-require("dotenv").config()
+const { ethers, network, run } = require('hardhat');
+const { deployPancakeswapExchange } = require('./deploy-pancakeswap-exchange');
+const { deployERC20ForSPLMintable } = require('./deploy-tokens');
+const { createPairAndAddLiquidity } = require('./create-liquidity-pools');
+const { airdropNEON } = require('./utils');
+require('dotenv').config();
 
 async function main() {
-    await run("compile")
+  await run('compile');
 
-    console.log("\nNetwork name: " + network.name)
+  console.log('\nNetwork name: ' + network.name);
 
-    if (!process.env.DEPLOYER_KEY) {
-        throw new Error("\nMissing private key: DEPLOYER_KEY")
-    }
+  if (!process.env.DEPLOYER_KEY) {
+    throw new Error('\nMissing private key: DEPLOYER_KEY');
+  }
 
-    const deployer = (await ethers.getSigners())[0]
-    console.log("\nDeployer address: " + deployer.address)
+  const deployer = (await ethers.getSigners())[0];
+  console.log('\nDeployer address: ' + deployer.address);
 
-    let deployerBalance = BigInt(await ethers.provider.getBalance(deployer.address))
-    const minBalance = ethers.parseUnits("10000", 18) // 10000 NEON
-    if(
-        deployerBalance < minBalance &&
-        parseInt(ethers.formatUnits((minBalance - deployerBalance).toString(), 18)) > 0
-    ) {
-        await airdropNEON(deployer.address, parseInt(ethers.formatUnits((minBalance - deployerBalance).toString(), 18)))
-        deployerBalance = BigInt(await ethers.provider.getBalance(deployer.address))
-    }
-    console.log("\nDeployer balance: " + ethers.formatUnits(deployerBalance.toString(), 18) + " NEON")
+  let deployerBalance = BigInt(await ethers.provider.getBalance(deployer.address));
+  const minBalance = ethers.parseUnits('10000', 18); // 10000 NEON
+  if (
+    deployerBalance < minBalance &&
+    parseInt(ethers.formatUnits((minBalance - deployerBalance).toString(), 18)) > 0
+  ) {
+    await airdropNEON(deployer.address, parseInt(ethers.formatUnits((minBalance - deployerBalance).toString(), 18)));
+    deployerBalance = BigInt(await ethers.provider.getBalance(deployer.address));
+  }
+  console.log('\nDeployer balance: ' + ethers.formatUnits(deployerBalance.toString(), 18) + ' NEON');
 
-    // Deploy Pancakeswap exchnage contracts and WNEON
-    const pancakeExchangeAddresses = await deployPancakeswapExchange(deployer)
+  // Deploy Pancakeswap exchnage contracts and WNEON
+  const pancakeExchangeAddresses = await deployPancakeswapExchange(deployer);
 
-    const mintAuthority = deployer.address // Set deployer as mint authority
+  const mintAuthority = deployer.address; // Set deployer as mint authority
 
-    // Deploy TOKEN_A (ERC20ForSPLMintable)
-    const tokenAAddress = await deployERC20ForSPLMintable(
-        "token_A",
-        "Token A",
-        "TOKEN_A",
-        9,
-        mintAuthority
-    )
+  // Deploy TOKEN_A (ERC20ForSPLMintable)
+  const tokenAAddress = await deployERC20ForSPLMintable(
+    'token_A',
+    'Token A',
+    'TOKEN_A',
+    9,
+    mintAuthority
+  );
 
-    // Deploy TOKEN_B (ERC20ForSPLMintable)
-    const tokenBAddress = await deployERC20ForSPLMintable(
-        "token_B",
-        "Token B",
-        "TOKEN_B",
-        12,
-        mintAuthority
-    )
+  // Deploy TOKEN_B (ERC20ForSPLMintable)
+  const tokenBAddress = await deployERC20ForSPLMintable(
+    'token_B',
+    'Token B',
+    'TOKEN_B',
+    12,
+    mintAuthority
+  );
 
-    // Create WNEON-TOKEN_A pair and provide liquidity
-    await createPairAndAddLiquidity(
-        pancakeExchangeAddresses.pancakeFactoryAddress,
-        pancakeExchangeAddresses.pancakeRouterAddress,
-        deployer,
-        pancakeExchangeAddresses.WNEONAddress,
-        tokenAAddress,
-        2000,
-        7333
-    )
+  // Create WNEON-TOKEN_A pair and provide liquidity
+  await createPairAndAddLiquidity(
+    pancakeExchangeAddresses.pancakeFactoryAddress,
+    pancakeExchangeAddresses.pancakeRouterAddress,
+    deployer,
+    pancakeExchangeAddresses.WNEONAddress,
+    tokenAAddress,
+    2000,
+    7333
+  );
 
-    // Create TOKEN_A-TOKEN_B pair and provide liquidity
-    await createPairAndAddLiquidity(
-        pancakeExchangeAddresses.pancakeFactoryAddress,
-        pancakeExchangeAddresses.pancakeRouterAddress,
-        deployer,
-        tokenAAddress,
-        tokenBAddress,
-        6345,
-        53809
-    )
+  // Create TOKEN_A-TOKEN_B pair and provide liquidity
+  await createPairAndAddLiquidity(
+    pancakeExchangeAddresses.pancakeFactoryAddress,
+    pancakeExchangeAddresses.pancakeRouterAddress,
+    deployer,
+    tokenAAddress,
+    tokenBAddress,
+    6345,
+    53809
+  );
 
-    console.log("\n")
+  console.log('\n');
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
