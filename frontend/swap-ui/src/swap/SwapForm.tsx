dc9eb9b3e19221c659de76319dcb35ceb4faf84c
Igor Prokopev <ip@neonlabs.org>
2025-03-20 20:47:33 +0700

    Returned gas fee estimation for devnet

--- frontend/swap-ui/src/swap/SwapForm.tsx
+++ frontend/swap-ui/src/swap/SwapForm.tsx
@@ -165,13 +165,7 @@ export const SwapForm: React.FC = (props: Props) => {
       chainId
     };
     const transactions = await dataMethod(params);
-    // remove after devnet/mainnet proxy releases
-    const { maxPriorityFeePerGas, maxFeePerGas } = await proxyApi.getMaxFeePerGas();
-    const gasLimit = transactions.map(_ => 1e7);
-    const transactionGas: TransactionGas = { maxPriorityFeePerGas, maxFeePerGas, gasLimit };
-    return swapMethodOld({ ...params, transactionGas });
-    // uncomment after devnet/mainnet proxy release
-/*    const approveInstruction = await approveMethod(connection, solanaUser, neonEvmProgram, tokenFrom, amountFrom);
+    const approveInstruction = await approveMethod(connection, solanaUser, neonEvmProgram, tokenFrom, amountFrom);
     const preparatorySolanaTransactions: PreparatorySolanaTransaction[] = [];
     const instructions: TransactionInstruction[] = [];
     if (approveInstruction) {
@@ -186,7 +180,7 @@ export const SwapForm: React.FC = (props: Props) => {
       preparatorySolanaTransactions
     });
     const method = (params: SwapTokenCommonData) => swapMethod(params, transactions, transactionGas, instructions);
-    return method({ ...params, transactionGas });*/
+    return method({ ...params, transactionGas });
   };
 
   const cancelTransaction = async (_: ScheduledTransactionStatus) => {
